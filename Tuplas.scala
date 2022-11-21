d3bcd9cec2728f884cc24e6ae3518ecfc64ed387
JSebastian2002 <jspenarreta@utpl.edu.ec>
2022-11-21 15:42:54 -0500

    Tarea

--- Tuplas.scala
+++ Tuplas.scala
@@ -20,7 +20,12 @@ def masgl1(Lst: List[(String, Int)])=
   print(masgl1(equipoft))
 
 def masgl2 (Lst: List[(String, Int)]) = 
-  lst.filter(a => a._2 > 16).map { case (int, -, -,gl) => (int, gl)}.sorted
+  lst.filter(a => a._2 > 16).map { case (int, _, _,gl) => (int, gl)}.sorted
   print(masgl2(equipoft))
 
+def jugador (Lst: List[(String, Int)]) = {
+  val lista2 = Lst.map{ case (int, _, _, gl) => (int, gp) }
+  groupBy(_._1).map {case (int, gp2) => (int, gp2.map(_._1).size)}
+  lista2.filter(a => a._2 == lista2.map(_._2).max)
+}
 
